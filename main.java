ab6ee32495d8fb29c1f69a4a628933874bda7328
Marvish Subhas Chandra <mschandr@uci.edu>
2022-11-25 09:39:44 -0800

    Add files via upload

---
+++ main.java
@@ -0,0 +1,12 @@
+// Marvish Chandra
+
+public static void registeredBooks(string[] name,book,issuedDays,afterExpiration){
+    String givenName = name;
+    System.out.println(givenName.toUpperCase());
+    String rentedBook = book;
+    System.out.println(rentedBook.toUpperCase());
+    int givenDays = issuedDays;
+    System.out.println(issuedDays);
+    int nonreturnedDays = afterExpiration;
+    System.println(nonreturnedDays);
+}
